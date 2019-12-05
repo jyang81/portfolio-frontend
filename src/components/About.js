864322e54e09161afb1ebb5893cc8929ed2f843d
jyang81 <joe.yang.design@gmail.com>
2019-12-04 23:37:45 -0800

    changed layout to flexbox

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ const About = () => {
     return (
         <main>
             <h2>About Me</h2>
+            <p>I'm a graphic designer turned software developer who is driven by _____, believes in ______, and has a passion for ________. </p>
         </main>
     );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,17 +6,15 @@ const Nav = () => {
         <nav>
             
             <a href="#home"><img src="logo192.png" className="logo" alt="Joe Yang logo" /></a>
-        
+
+            <a href="#about">About</a> 
+            <a href="#projects">Projects</a> 
+            <a href="#tech">Technologies</a> 
+            <a href="#contact">Contact</a> 
+            {/* <a href="javascript:void(0);" class="icon" >
+                <i class="fa fa-bars"></i>
+            </a> */}
             
-            <div>
-                <a href="#about">About</a> 
-                <a href="#projects">Projects</a> 
-                <a href="#tech">Technologies</a> 
-                <a href="#contact">Contact</a> 
-                <a href="javascript:void(0);" class="icon" >
-                    <i class="fa fa-bars"></i>
-                </a>
-            </div>
         </nav>
     )
 }
