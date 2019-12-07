e64ac2e3ad6c9af5d777e172cc66dd9809132b43
jyang81 <joe.yang.design@gmail.com>
2019-12-07 14:52:18 -0800

    add section anchors, top spacer

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const About = () => {
     return (
+        <a name="about">
         <main className="light-bg">
             <h2>About Me</h2>
-            <p>I'm a graphic designer turned software developer who is driven by _____, believes in ______, and has a passion for ________. </p>
+            <p>I'm a graphic designer turned software developer who is driven by _____, believes in ______, and has a passion for ________. Lorem ipsum Lorem ipsum Lorem ipsum. Some more stuff about me. This is my journey. Thanks for reading. Sample text over.</p>
         </main>
+        </a>
     );
 };
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Home = () => {
     return (
         <main>
-            <img src="myAvatar.png" alt="avatar" className="mainimage" />
-            <h1>Joe Yang</h1>
-            <h3>Software Developer</h3>
+          <img src="myAvatar.png" alt="avatar" className="mainimage" />
+          <br />
+          <h1>Joe Yang</h1>
+          <h3>Software Developer</h3>
         </main>
     );
 };
